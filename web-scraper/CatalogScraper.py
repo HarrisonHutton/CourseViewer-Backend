c5559c6dc762f8584b6c079369c1b7e83a570f16
HarrisonHutton <harrison.hutton6@gmail.com>
2022-11-01 09:47:35 -0400

    ✨ Write CatalogScraper class

---
+++ web-scraper/CatalogScraper.py
@@ -0,0 +1,30 @@
+"""
+Since UB's catalog page data is sourced from a public json file, we can use that to get the
+catalog URLs for all the departments.
+
+Source: https://catalog.buffalo.edu/courses/data/subjects.json
+"""
+
+from models.CatalogInfo import CatalogInfo
+import requests
+import json
+
+class CatalogScraper:
+
+    def get_catalog_urls(self):
+        # Get the json data from the UB catalog page
+        page = requests.get("https://catalog.buffalo.edu/courses/data/subjects.json")
+        data = json.loads(page.content)
+
+        # The json data is a list of dictionaries, where each dictionary contains the department
+        # code, department name, and the URL of the department's catalog page in the following format:
+        #
+        # {"page": <dpt_name>, "url": <catalog_url>, "abbr": <dpt_code>}
+        catalog_urls = []
+
+        for dpt in data:
+            url = "https://catalog.buffalo.edu/courses/" + dpt["url"]
+            info = CatalogInfo(dpt["abbr"], dpt["page"], url)
+            catalog_urls.append(info)
+
+        return catalog_urls

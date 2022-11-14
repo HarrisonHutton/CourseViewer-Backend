c08efc97b6f6c1b646186ffa7e314a0e3cc7bf54
HarrisonHutton <harrison.hutton6@gmail.com>
2022-11-14 16:29:26 -0500

    ✨ Write to_dict()

--- web_scraper/models/CourseInfo.py
+++ web_scraper/models/CourseInfo.py
@@ -13,6 +13,23 @@ class CourseInfo:
     typically_offered: list[str]
     requisites: str = None
 
+    """
+    Create a dictionary from the CourseInfo object.
+    """
+    def to_dict(self):
+        return {
+            "dpt_code": self.dpt_code,
+            "course_num": self.course_num,
+            "course_name": self.course_name,
+            "course_type_abbr": self.course_type_abbr,
+            "course_type": self.course_type,
+            "course_description": self.course_description,
+            "credits": self.credits,
+            "grading": self.grading,
+            "typically_offered": self.typically_offered,
+            "requisites": self.requisites,
+        }
+
 @dataclass
 class CourseTitle:
     dpt_code: str
